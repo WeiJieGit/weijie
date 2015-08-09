23fe73b164018d364b6c8e5641a9f94a5b4d02be
WeiJieGit <guoweijie@WJMacBook-Pro.local>
2015-08-09 16:17:45 +0800

    ns

---
+++ gitDemo/gitDemo/demo.h
@@ -0,0 +1,13 @@
+//
+//  demo.h
+//  gitDemo
+//
+//  Created by 郭伟杰 on 15/8/9.
+//  Copyright (c) 2015年 GWJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface demo : NSObject
+
+@end
---
+++ gitDemo/gitDemo/demo.m
@@ -0,0 +1,13 @@
+//
+//  demo.m
+//  gitDemo
+//
+//  Created by 郭伟杰 on 15/8/9.
+//  Copyright (c) 2015年 GWJ. All rights reserved.
+//
+
+#import "demo.h"
+
+@implementation demo
+
+@end
